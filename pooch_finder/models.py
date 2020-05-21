92fd94b298bf885187efe7e877ce49c7da52dc0b
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-05-21 16:30:00 -0600

    add breed model

--- pooch_finder/models.py
+++ pooch_finder/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings as conf_settings
+from datetime import datetime 
+
+
+# Create your models here.
+class Breed(models.Model):
+    name = models.CharField(max_length=64)
