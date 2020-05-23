3e6b2fb1d871472736c24cb08be0b837bc79c36b
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-05-23 17:30:00 -0600

    add logout view

--- pooch_finder/views.py
+++ pooch_finder/views.py
@@ -75,3 +75,10 @@ def register_view(request):
     else:
         return render(request, "users/register.html", {"message":None})
     
+
+# logout view
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {"message": None})
+
+
