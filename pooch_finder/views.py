9cb54c052117ecc91865f247cea987c8bfc75888
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-05-23 17:30:00 -0600

    add index view

--- pooch_finder/views.py
+++ pooch_finder/views.py
@@ -88,3 +88,110 @@ def forgot_password(request):
         return password_reset(request, from_email=request.POST.get('email'))
     else:
         return render(request, 'users/forgot_password.html')
+
+#index page
+@login_required(login_url='login')
+def index(request):
+    try:
+        ad_items = None
+        context = {}
+        # if user filter results
+        if request.method == "POST":
+            state = request.POST["state"]
+            # filter by state
+            if state and state != "ALL":
+                ad_items = Ad_Item.objects.filter(active = True, item_location__state = state)
+                context.update(state_val = state )
+            else:
+                ad_items = Ad_Item.objects.filter(active = True)
+            
+            # filter by location and within given kms of distance
+            # for this functionality had to use sql math functions extention
+            location_str = request.POST["location"]
+            if location_str:
+                location_data  = location_str.split(", ")
+                if len(location_data) == 3:
+                    location= Location.objects.get(Q(postcode=location_data[2]) & Q(suburb=location_data[0]) & Q(state=location_data[1]))
+                    if location:
+                        context.update(location = location )
+                        
+                        distance = request.POST["distance"]
+                        context.update(distance = int(distance) )
+                        
+                        conn = sqlite3.connect(os.path.join(conf_settings.BASE_DIR, 'db.sqlite3'))
+                        conn.enable_load_extension(True)
+                        conn.load_extension("./math")
+                        c = conn.cursor()
+                        c.execute('Select id From pooch_finder_location Where acos(sin(radians(?))*sin(radians(lat)) + cos(radians(?))*cos(radians(lat))*cos(radians(lon)- radians(?))) * 6371 < ? and lat != 0.0 and lon !=0.0', (location.lat, location.lat, location.lon, int(distance)))
+                        
+                        rows = c.fetchall()
+                        surrounding_subs = [i[0] for i in rows]
+                        conn.enable_load_extension(False)
+                        conn.close()
+                        ad_items = ad_items.filter(item_location__id__in = surrounding_subs)
+                        
+            # filter by price range
+            price_range = request.POST["price_range"]
+            if price_range:
+                ad_items = ad_items.filter(price__lte = float(price_range))
+                context.update(price_range = price_range )
+            # filter by age    
+            age = request.POST["age"]
+            if age and age != "ALL":
+                ad_items = ad_items.filter(age__id = age)
+                context.update(age_val = age )
+            # filter by breed    
+            breed = request.POST["breed"]
+            if breed and breed != "ALL":
+                ad_items = ad_items.filter(breed__id = breed)
+                context.update(breed_val = breed )
+            # filter by gender
+            gender = request.POST["gender"]
+            if gender and gender != "ALL":
+                ad_items = ad_items.filter(gender__id = gender)
+                context.update(gender_val = gender)
+            
+            # sort by most recent(MR) or Alphabetical order(AO) or Most Expensive(ME) or Cheapest(C)
+            sort = request.POST["sort"]
+            if sort:
+                context.update(sort_val = sort )
+                if sort == "MR":
+                    ad_items = ad_items.order_by('-date_time')
+                if sort == "AO":
+                    ad_items = ad_items.order_by('title')
+                if sort == "ME":
+                    ad_items = ad_items.order_by('-price')
+                if sort == "C":
+                    ad_items = ad_items.order_by('price')
+                
+                
+        else:
+            # get all active ads
+            ad_items = Ad_Item.objects.filter(active = True)
+        
+        # pagination
+        paginator = Paginator(ad_items, conf_settings.NO_OF_ADS_PER_PAGE)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        
+        dict_photo_items ={}
+        
+        for ad_item in page_obj:
+            # laod photos for each ad item
+            photo_items = Picture.objects.filter(ad_item =ad_item)
+            if photo_items:
+                dict_photo_items[ad_item.id] = photo_items
+    except Ad_Item.DoesNotExist:
+        return render(request, "error.html", {"message": "Ad items does not exist."})
+    except Picture.DoesNotExist:
+        return render(request, "error.html", {"message": "photo items does not exist."})
+    
+    # load all below details to populate filter section
+    states = State.objects.all().order_by('name')
+    breeds = Breed.objects.all().order_by('name')
+    genders = Gender.objects.all()
+    ages = Age_Cat.objects.all()
+    
+    context.update({'page_obj': page_obj, "photo_items":dict_photo_items, "states":states,"breeds":breeds,"genders":genders,"ages":ages,})
+    
+    return render(request, "index.html", context)
