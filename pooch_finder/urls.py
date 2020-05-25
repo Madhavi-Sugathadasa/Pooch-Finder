e70fc82648af54796520c628f43d13af935d7955
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-05-24 18:40:00 -0600

    update urls

--- pooch_finder/urls.py
+++ pooch_finder/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path("register", views.register_view, name="register"),
     path("logout", views.logout_view, name="logout"),
     path("post_ad", views.post_ad, name="post_ad"),
+    path("<int:ad_id>", views.ad_more_details, name="ad_more_details"),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
