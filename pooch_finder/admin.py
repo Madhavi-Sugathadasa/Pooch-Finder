28fb388c96dc10a8055e6f3e520fd091d4eb31ec
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-05-25 18:52:00 -0600

    update admin

--- pooch_finder/admin.py
+++ pooch_finder/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import Breed, Dog_Type, Age_Cat, Gender, Picture, Location, Ad_Item, State
+
+# Register your models here.
+admin.site.register(Breed)
+admin.site.register(Dog_Type)
+admin.site.register(Age_Cat)
+admin.site.register(Gender)
+admin.site.register(Picture)
+admin.site.register(Location)
+admin.site.register(Ad_Item)
+admin.site.register(State)
