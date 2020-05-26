ad033ca93e2c6a7b5ad6cf29dac53eb4e89103be
Madhavi-Sugathadasa <madhavi.sug@gmail.com>
2020-05-25 18:46:00 -0600

    update urls

--- pooch_finder/urls.py
+++ pooch_finder/urls.py
@@ -19,6 +19,7 @@ urlpatterns = [
     path("autocomplete_location", views.autocomplete_location, name="autocomplete_location"),
     path("edit_ad/<int:ad_id>", views.edit_ad, name="edit_ad"),
     path("send_message", views.send_message, name="send_message"),
+    path("delete_pic/<int:ad_id>/<int:pic_id>", views.delete_pic, name="delete_pic"),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
